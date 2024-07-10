8b11ea066126dff4ff4b6c0c8bb9d61370dc9cfb
Kevin Karahali <kevin.karahali@hotmail.com>
2024-07-10 12:10:29 +0200

    Add home page and configure views and URLs for polls

--- djangoTutorial/urls.py
+++ djangoTutorial/urls.py
@@ -15,8 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.http import HttpResponse
+from django.urls import path, include
+
+
+def home(request):
+    return HttpResponse("Welcome to the Home Page!")
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path("polls/", include("polls.urls")),
 ]
