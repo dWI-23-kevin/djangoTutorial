36584a980b7690c6888766294ffb767073240c2f
Kevin Karahali <kevin.karahali@hotmail.com>
2024-08-01 10:09:40 +0200

    Part 8

--- djangoTutorial/settings.py
+++ djangoTutorial/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -46,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'djangoTutorial.urls'
@@ -107,6 +110,10 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
--- djangoTutorial/urls.py
+++ djangoTutorial/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.http import HttpResponse
 from django.urls import path, include
@@ -28,3 +29,13 @@ urlpatterns = [
     path('', home, name='home'),
     path("polls/", include("polls.urls")),
 ]
+
+# if settings.DEBUG:
+#     import debug_toolbar
+#
+#     urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
